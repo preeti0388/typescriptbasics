c71216156b7afdb4b70bbf142fe24e6b024b17dc
preeti0388 <preeti0388@gmail.com>
2019-07-06 11:36:34 +1000

    used git stash and stash pop

--- App.js
+++ App.js
@@ -15,3 +15,12 @@ array.forEach((key,item,arr)=>{
     console.log(arr);
     console.log(item, index,arr);
   })
+
+  // JavaScript Variables example
+  for (var i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 1); //3 3 3
+  }
+  
+  for (let i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 1); //0 1 2
+  }
