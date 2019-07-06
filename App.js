2ee3c373f6e9580940293042140df2e164aec1f1
preeti0388 <preeti0388@gmail.com>
2019-07-06 11:06:26 +1000

    foreach parameters

--- App.js
+++ App.js
@@ -2,3 +2,16 @@ const array = ["one", "two", "three"]
 array.forEach((item, index,arr)=> {
   console.log(item, index,arr);
 });
+
+const array = ["one", "two", "three"]
+array.forEach((item, index,arr)=> {
+  console.log(item, index,arr);
+});
+
+
+array.forEach((key,item,arr)=>{
+    console.log(key);
+    console.log(item);
+    console.log(arr);
+
+  })
